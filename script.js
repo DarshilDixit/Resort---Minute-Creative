b2c77461fc9a4403aadd8fdea46e55ad9d906985
Darshil Dixit <darshil@darshildixit.com>
2025-11-03 18:40:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 console.log("Hello, World! I am Darshil Dixit.");
 console.log("I am learning JavaScript.");
 console.log("I love coding and building projects.");
+console.log("This is my first script file.");
