6e8394a2dc4f377bc1c127427631d6664e523d27
Darshil Dixit <darshil@darshildixit.com>
2025-11-04 00:36:33 +0530

    update
    another

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ console.log("Hello, World! I am Darshil Dixit.Welcome to my Resort Website.");
 console.log("Enjoy your stay at our beautiful resort!");
 console.log("Feel free to explore and have a great time!");
 console.log("Contact us for any assistance during your stay.");
+console.log("Thank you for choosing our resort. We hope to see you again!");
